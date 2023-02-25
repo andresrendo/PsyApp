bc0e6bf0f345d55c511686386235cd978f447020
Andres Rendo <andres.rendo@correo.unimet.edu.ve>
2023-02-25 14:16:20 -0400

    navbar0.2

--- ProyectoSist/src/App.jsx
+++ ProyectoSist/src/App.jsx
@@ -1,22 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import Navbar from './components/navbar/Navbar'
+import TopNav from './components/navbar/TopNav.jsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <div className="PsyApp">
       <div>
-        <Navbar/>
+        <TopNav/>
       </div>
-      <h1>PsyApp</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      
     </div>
   );
 }
---
+++ ProyectoSist/src/components/navbar/TopNav.jsx
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+// import styles from "./Navbar.module.css"
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+
+
+function TopNav() {
+  return (
+    <Navbar className='navbar' bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default TopNav

2c088f6cf1b21e206254a44034a3464e690ce13b
Juan Araque <juan.araque@correo.unimet.edu.ve>
2023-03-15 02:45:02 -0400

    estilos completos login y registro

--- ProyectoSist/src/pages/LoginPage/Login.jsx
+++ ProyectoSist/src/pages/LoginPage/Login.jsx
@@ -8,29 +8,35 @@ export const Login = () => {
     };
 
     return(
-        <div className='d-flex justify-content-evenly flex-wrap'>
+        <div className={`d-flex justify-content-evenly flex-wrap align-items-center ${styles.loginContainer}`}>
             <div className='img-container'>
                 <img src="https://clinicavicent.com/wp-content/uploads/2021/06/psicologia-educativa-1.png" className={styles.image} />
             </div>
 
 
-            <div className='pt-5'>
+            <div className='my-5'>
                 {/* <Link to={REGISTER_URL}>¿No tienes cuenta?{" "} <span>Registrate</span></Link> */}
                 <span>¿No tienes cuenta? <a href={REGISTER_URL} className={styles.anchor}>Registrate aqui{" "}</a></span>
-                <h2 className="mt-3 mb-3">Iniciar sesión</h2>
+                <h2 className={`mt-3 mb-3 ${styles.loginTitle}`}>Iniciar sesión</h2>
                 <form>
                     <div className="mb-3">
-                        <label className="form-label">Correo</label>
-                        <input type="email" className="form-control" id="InputEmail1" aria-describedby="emailHelp"/>
+                        <label className={`form-label`}>Correo</label>
+                        <input type="email" className={`form-control ${styles.input}`} id="InputEmail1" aria-describedby="emailHelp"/>
                     </div>
                     <div className="mb-4">
-                        <label className="form-label">Contraseña</label>
-                        <input type="password" className="form-control" id="InputPassword1"/>
+                        <label className={`form-label`}>Contraseña</label>
+                        <input type="password" className={`form-control ${styles.input}`} id="InputPassword1"/>
                     </div>
                     <button type="submit" className={`btn btn-primary ${styles.button1}`}>Iniciar</button>
                 </form>
 
-                    <button type="button" onClick={handleSignWithGoogle} className={`btn btn-secondary pb-2 ${styles.button2}`}>Continuar con Google</button>
+                    <div className={`d-flex align-items-center justify-content-between mb-3 ${styles.containerDivisor}`}>
+                        <div className={styles.division}></div>
+                        O
+                        <div className={styles.division}></div>
+                    </div>
+
+                    <button type="button" onClick={handleSignWithGoogle} className={`btn btn-secondary pb-2 ${styles.button2}`}>Continuar con <i class="fa-brands fa-google"></i>oogle</button>
 
             </div>
         </div>
--- ProyectoSist/src/pages/RegisterPage/Register.jsx
+++ ProyectoSist/src/pages/RegisterPage/Register.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { LOGIN_URL } from "../../constants/urls";
 import { signInWithGoogle } from "../../firebase/auth-service";
 import styles from "./Register.module.css"
@@ -25,52 +26,60 @@ export const Register = () => {
     }
 
     return(
+
         <div className='d-flex justify-content-evenly flex-wrap'>
             <div className='d-flex align-items-center'>
                 <img src="https://agsmedico.com/img/especialidades/Psicologia.png" className={styles.image}/>
             </div>
 
             <div className="pt-5 px-3">
-                <h2>Registrarse</h2>
+                <h2 className={styles.registerTitle}>Registrarse</h2>
                 <p>¿Ya tienes una cuenta? <a href={LOGIN_URL} className={styles.anchor}>Accede aqui</a></p>
                 
                 <form className={styles.formFormat}>
                     <div className="mb-3">
                         <label className="form-label">Nombre</label>
-                        <input type="text" className="form-control" id="InputName" name="name"/>
+                        <input type="text" className={`form-control ${styles.input}`} id="InputName" name="name"/>
                     </div>
                     <div className="mb-3">
                         <label className="form-label">Apellido</label>
-                        <input type="text" className="form-control" id="InputName" name="lastName"/>
+                        <input type="text" className={`form-control ${styles.input}`} id="InputName" name="lastName"/>
                     </div>
 
                     <div className="mb-3">
                         <label className="form-label">Correo</label>
-                        <input type="email" className="form-control" id="InputEmail1" name="email" aria-describedby="emailHelp"/>
+                        <input type="email" className={`form-control ${styles.input}`} id="InputEmail1" name="email" aria-describedby="emailHelp"/>
                     </div>
                     <div className="mb-3">
                         <label className="form-label">Contraseña</label>
-                        <input type="password" className="form-control" id="InputPassword1" name="password"/>
+                        <input type="password" className={`form-control ${styles.input}`} id="InputPassword1" name="password"/>
                     </div>
                     <div className="mb-3">
                         <label className="form-label">Confirmar contraseña</label>
-                        <input type="password" className="form-control" id="InputPassword1"/>
+                        <input type="password" className={`form-control ${styles.input}`} id="InputPassword1"/>
                     </div>
                     
                     <div class="input-group mb-3 d-flex justify-content-evenly">
                         <div class="input-group-text">
-                            <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input" />
-                            <label className="ms-1">Paciente</label>
+                            <input class="form-check-input mt-0" type="radio" id="paciente" value="Paciente" name="tipoUsuario" aria-label="paciente" />
+                            <label className="ms-2">Paciente</label>
                         </div>
                         <div class="input-group-text">
-                            <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input" />
-                            <label className="ms-1">Doctor</label>
+                            <input class="form-check-input mt-0" type="radio" id="doctor" value="Doctor" name="tipoUsuario" aria-label="doctor" />
+                            <label className="ms-2">Doctor</label>
                         </div>
                     </div>
                     <div className="d-flex justify-content-center">
-                        <button type="submit" className={`btn btn-primary py-2 ${styles.registerButton}`}>Registrarse</button>
+                        <button type="submit" className={`btn btn-primary py-2 mt-2 ${styles.registerButton}`}>Registrarse</button>
                     </div>
-                    <button type="button" onClick={handleSignWithGoogle} className={`btn btn-secondary mt-2 py-2 mb-5 ${styles.button2}`}>Registrate con Google</button>
+
+                    <div className={`d-flex align-items-center justify-content-between my-2 ${styles.containerDivisor}`}>
+                        <div className={styles.division}></div>
+                        O
+                        <div className={styles.division}></div>
+                    </div>
+
+                    <button type="button" onClick={handleSignWithGoogle} className={`btn btn-secondary mt-2 py-2 mb-5 ${styles.button2}`}>Continuar con <i class="fa-brands fa-google"></i>oogle</button>
                     
                 </form>                    
             </div>

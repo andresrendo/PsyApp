0b75e13b71b208f49ea60d526789170bc4f6b28b
SebasMarquez <sebastian.marquez@correo.unimet.edu.ve>
2023-03-27 18:25:20 -0400

    config psyapp prueba

--- ProyectoSist/src/firebase/config.js
+++ ProyectoSist/src/firebase/config.js
@@ -1,5 +1,36 @@
+
+//Configuracion de psyapp
 // Import the functions you need from the SDKs you need
 
+//import { initializeApp } from "firebase/app";
+//import{ GoogleAuthProvider ,getAuth } from "firebase/auth";
+//import { getFirestore } from "firebase/firestore";
+//import { getStorage } from "firebase/storage";
+//// TODO: Add SDKs for Firebase products that you want to use
+//// https://firebase.google.com/docs/web/setup#available-libraries
+//
+//// Your web app's Firebase configuration
+//const firebaseConfig = {
+//  apiKey: "AIzaSyAMED3UtcE-GfMjeJ2K5on-LXkzvmggGJE",
+//  authDomain: "psyapp-722c0.firebaseapp.com",
+//  projectId: "psyapp-722c0",
+//  storageBucket: "psyapp-722c0.appspot.com",
+//  messagingSenderId: "1010699773060",
+//  appId: "1:1010699773060:web:9a098a87df80aba438ced1"
+//};
+//
+//// Initialize Firebase
+//const app = initializeApp(firebaseConfig);
+//
+//export const auth = getAuth(app);
+//export const db = getFirestore(app);
+//export const storage = getStorage(app);
+//
+//export const googleProvider = new GoogleAuthProvider();
+//googleProvider.setCustomParameters({ prompt: "select_account" });
+
+//Configuracion de psyapp prueba
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import{ GoogleAuthProvider ,getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -8,13 +39,15 @@ import { getStorage } from "firebase/storage";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAMED3UtcE-GfMjeJ2K5on-LXkzvmggGJE",
-  authDomain: "psyapp-722c0.firebaseapp.com",
-  projectId: "psyapp-722c0",
-  storageBucket: "psyapp-722c0.appspot.com",
-  messagingSenderId: "1010699773060",
-  appId: "1:1010699773060:web:9a098a87df80aba438ced1"
+  apiKey: "AIzaSyC04b02ef9xC7H7h_GnHIfwEe65rt1f6IQ",
+  authDomain: "psyapp-prueba.firebaseapp.com",
+  projectId: "psyapp-prueba",
+  storageBucket: "psyapp-prueba.appspot.com",
+  messagingSenderId: "132513303818",
+  appId: "1:132513303818:web:e3e14d1c2cf3cf9043bef3",
+  measurementId: "G-B69ERLVDW3"
 };
 
 // Initialize Firebase
